30c1380d0d2778728d969927aa724103ab49dd46
SenneVanderAuwera <senne.vanderauwera@telenet.be>
2020-05-05 19:05:53 +0200

    Added searchbar

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/index/navbar.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <title>PHP Test</title>
+    </head>
+    <body>
+        <nav class="topnav">
+            <input class="searchBar" placeholder="Search">
+        </nav>
+    </body>
+</html>
