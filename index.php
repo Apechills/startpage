d9ebe4116d77d7517bc498419d4c4be6c92c4f04
SenneVanderAuwera <senne.vanderauwera@telenet.be>
2020-05-08 18:01:10 +0200

    Page layout

--- index.php
+++ index.php
@@ -3,12 +3,60 @@
     <head>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/index/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/index/container.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <title>PHP Test</title>
+    <title>Startpage</title>
     </head>
     <body>
         <nav class="topnav">
             <input class="searchBar" placeholder="Search">
         </nav>
+        <div class="contentWrapper">
+            <ul class="linkContainer">
+                <header class="linkContainerHeader">Finance</header>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+            </ul>
+            <ul class="linkContainer">
+                <header class="linkContainerHeader">Finance</header>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+            </ul>
+            <ul class="linkContainer">
+                <header class="linkContainerHeader">Finance</header>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+            </ul>
+            <ul class="linkContainer">
+                <header class="linkContainerHeader">Finance</header>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+            </ul>
+            <ul class="linkContainer">
+                <header class="linkContainerHeader">Finance</header>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+                <li class="linkLi"><a href="" class="linkA">Link1</a></li>
+            </ul>
+        </div>
     </body>
 </html>
