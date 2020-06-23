c6d5af3c909b6beee15ab867d646df8a04278847
SenneVanderAuwera <senne.vanderauwera@telenet.be>
2020-06-23 17:46:22 +0200

    Settings styling

--- pages/settings.php
+++ pages/settings.php
@@ -3,16 +3,17 @@
     <head>
     <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="../css/index/container.css">
+    <link rel="stylesheet" type="text/css" href="../css/settings/settingsContainer.css">
+    <link rel="stylesheet" type="text/css" href="../css/settings/loginform.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <title>Startpage</title>
     </head>
     <body>
         <script>
             var activeSession = <?php echo $_COOKIE["active"]; ?>;
+            console.log(activeSession);
 
-        
-                console.log(activeSession);
+            
         </script>
 
         <nav class="topnav">
@@ -21,11 +22,14 @@
             </div>
         </nav>
         <div class="contentWrapper">
-            <form class="loginForm" action="../php/loginProcess.php" method="POST">
-                <input name="username" type="text" class="usernameInput">
-                <input name="password" type="password" class="passwordInput">
-                <button type="submit" class="loginSubmit">Submit</button>
-            </form>
+            <ul class="settingsContainer">
+                <header class="settingsContainerHeader">Account</header>
+                <form class="loginForm" action="../php/loginProcess.php" method="POST">
+                    <input placeholder="Username" name="username" type="text" class="usernameInput">
+                    <input placeholder="Password" name="password" type="password" class="passwordInput">
+                    <button type="submit" class="loginSubmit">Log in or register</button>
+                </form>
+            </ul>
         </div>
     </body>
 </html>
