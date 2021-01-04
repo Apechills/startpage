9250f59dfad68ba55871f2774d70ac896a198efa
KrisVda1 <Kris.Vanderauwera@telenet.be>
2021-01-04 11:09:40 +0100

    Add files via upload
    Adding support for loading pages with API key
    Special chars to indicatie api page: // (double slash)

---
+++ contentLoading.php
@@ -0,0 +1,33 @@
+<?php
+require('connect.inc.php');
+$uid = $_COOKIE["uid"];
+
+$loadContainersSql = "SELECT * FROM containers WHERE userid = $uid AND deleted=0";
+
+if($containerResult = mysqli_query($con, $loadContainersSql)) {
+    while($containerRow = mysqli_fetch_assoc($containerResult)) {
+        $containerId = $containerRow['container_id'];
+        $containerHeader = $containerRow['container_header'];
+
+        echo '  <ul id="'.$containerId.'" class="linkContainer">
+                <div class="editOverlay hidden"><img class="overlayIcon" src="_assets/edit-24px.svg"></div>
+                <header class="linkContainerHeader">'.$containerHeader.'<input disabled hidden value="'.$containerId.'"></header>';
+
+        $loadItemsSql = "SELECT * FROM items WHERE container_link='$containerId' AND deleted=0;";
+
+        if($itemResult = mysqli_query($con, $loadItemsSql)) {
+            while($itemRow = mysqli_fetch_assoc($itemResult)) {
+                if($itemRow['item_name'] == "**todoIst") {
+                  //Perform action
+                    echo (file_get_contents($itemRow['item_href']));
+                  break;
+                } else {
+                  echo '<li class="linkLi"><a href="'.$itemRow['item_href'].'" class="linkA">'.$itemRow['item_name'].'</a></li>';
+                }
+            }
+        }
+
+        echo '</ul>';
+    }
+}
+?>
